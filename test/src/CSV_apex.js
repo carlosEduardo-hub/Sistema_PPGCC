d8657548050c05684a36bbcb6d7f244b67a29ed7
FerdRosa <fernando.a.rosa02@gmail.com>
2023-11-21 08:35:40 -0300

    Refazendo grafico padrao ApexCharts

--- test/src/CSV_apex.js
+++ test/src/CSV_apex.js
@@ -7,8 +7,8 @@ const CSVReader = () => {
   const [dataForApexCharts, setDataForApexCharts] = useState([]);
   const [infoData, setInfoData] = useState([]);
   const [dataColors, setDataColors] = useState({});
-  // const [selectedInfo, setSelectedInfo] = useState([]);
-  const [selectedInfo, setSelectedInfo] = useState('');
+  const [selectedInfo, setSelectedInfo] = useState([]);
+  // const [selectedInfo, setSelectedInfo] = useState('');
 
   const handleFileChosen = (file) => {
     Papa.parse(file, {
@@ -37,22 +37,22 @@ const CSVReader = () => {
         });
 
         setDataForApexCharts(formattedData);
-        // setSelectedInfo([formattedData[0].nome]); // Inicializa com o primeiro valor por padrão
-        setSelectedInfo(formattedData[0].nome); // Inicializa com o primeiro valor por padrão
+        setSelectedInfo([formattedData[0].nome]); // Inicializa com o primeiro valor por padrão
+        // setSelectedInfo(formattedData[0].nome); // Inicializa com o primeiro valor por padrão
       },
       header: true,
     });
   };
 // //ESTA PARA MULTIPLAS ESCOLHAS NECESSARIO MUDAR PARA AS OUTRAS
-  // const handleInfoChange = (e) => {
-  //   const selectedValues = Array.from(e.target.selectedOptions, (option) => option.value);
-  //   setSelectedInfo(selectedValues || []); // Certifique-se de que selectedInfo seja um array
-  // };
+  const handleInfoChange = (e) => {
+    const selectedValues = Array.from(e.target.selectedOptions, (option) => option.value);
+    setSelectedInfo(selectedValues || []); // Certifique-se de que selectedInfo seja um array
+  };
 
 // //ESTA PARA MULTIPLAS ESCOLHAS NECESSARIO MUDAR PARA AS OUTRAS
-const handleInfoChange = (selectedValue) => {
-  setSelectedInfo(selectedValue);
-};
+// const handleInfoChange = (selectedValue) => {
+//   setSelectedInfo(selectedValue);
+// };
 
   useEffect(() => {
     const input = document.createElement('input');
@@ -111,83 +111,83 @@ const handleInfoChange = (selectedValue) => {
       // );
   
 
-      // const options = {
-      //   xaxis: {
-      //     categories: infoData.filter((header) => header !== 'nome'),
-      //   },
-      //   colors: Object.values(dataColors),
-      // };
+      const options = {
+        xaxis: {
+          categories: infoData.filter((header) => header !== 'nome'),
+        },
+        colors: Object.values(dataColors),
+      };
     
-      // return (
-      //   <div>
-      //     <h2>Informação Headers:</h2>
-      //     <pre>{infoData.join(', ')}</pre>
+      return (
+        <div>
+          <h2>Informação Headers:</h2>
+          <pre>{infoData.join(', ')}</pre>
     
-      //     {infoData.length > 0 && (
-      //       <select multiple value={selectedInfo} onChange={handleInfoChange}>
-      //         {dataForApexCharts.map((item) => (
-      //           <option key={item.nome} value={item.nome}>
-      //             {item.nome}
-      //           </option>
-      //         ))}
-      //       </select>
-      //     )}
+          {infoData.length > 0 && (
+            <select multiple value={selectedInfo} onChange={handleInfoChange}>
+              {dataForApexCharts.map((item) => (
+                <option key={item.nome} value={item.nome}>
+                  {item.nome}
+                </option>
+              ))}
+            </select>
+          )}
     
-      //     <Chart
-      //       options={options}
-      //       series={selectedInfo.map((info) => ({
-      //         name: info,
-      //         data: infoData
-      //           .filter((header) => header !== 'nome')
-      //           .map((year) => dataForApexCharts.find((item) => item.nome === info)[year]),
-      //       }))}
-      //       type="bar"
-      //       height={350}
-      //     />
-      //   </div>
-      // );
+          <Chart
+            options={options}
+            series={selectedInfo.map((info) => ({
+              name: info,
+              data: infoData
+                .filter((header) => header !== 'nome')
+                .map((year) => dataForApexCharts.find((item) => item.nome === info)[year]),
+            }))}
+            type="bar"
+            height={350}
+          />
+        </div>
+      );
 
 
 // // FAZENDO O GRAFICO EM LINHA 
-const options = {
-  chart: {
-    id: 'line',
-    toolbar: {
-      show: false,
-    },
-  },
-  xaxis: {
-    categories: dataForApexCharts.map((item) => item.nome),
-  },
-};
+// const options = {
+//   chart: {
+//     id: 'line',
+//     toolbar: {
+//       show: false,
+//     },
+//   },
+//   xaxis: {
+//     categories: dataForApexCharts.map((item) => item.nome),
+//   },
+// };
 
-const filteredData = dataForApexCharts.filter((item) => item.nome === selectedInfo);
+// const filteredData = dataForApexCharts.filter((item) => item.nome === selectedInfo);
 
-  const series = infoData
-    .filter((header) => header !== 'nome')
-    .map((year) => ({
-      name: year,
-      data: filteredData.map((item) => item[year]),
-    }));
+//   const series = infoData
+//     .filter((header) => header !== 'nome')
+//     .map((year) => ({
+//       name: year,
+//       data: filteredData.map((item) => item[year]),
+//     }));
 
-return (
-  <div>
-    <h2>Informação Headers:</h2>
-    <pre>{infoData.join(', ')}</pre>
+// return (
+//   <div>
+//     <h2>Informação Headers:</h2>
+//     <pre>{infoData.join(', ')}</pre>
 
-    {infoData.length > 0 && (
-      <select value={selectedInfo} onChange={(e) => handleInfoChange(e.target.value)}>
-      {dataForApexCharts.map((item) => (
-        <option key={item.nome} value={item.nome}>
-          {item.nome}
-        </option>
-      ))}
-    </select>
-    )}
+//     {infoData.length > 0 && (
+//       <select value={selectedInfo} onChange={(e) => handleInfoChange(e.target.value)}>
+//       {dataForApexCharts.map((item) => (
+//         <option key={item.nome} value={item.nome}>
+//           {item.nome}
+//         </option>
+//       ))}
+//     </select>
+//     )}
 
-    <Chart options={options} series={series} type="line" height={600} />
-  </div>
-);
+//     <Chart options={options} series={series} type="line" height={600} />
+//   </div>
+// );
 
   
 
