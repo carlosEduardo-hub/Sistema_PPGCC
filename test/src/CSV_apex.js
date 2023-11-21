17268ee1c3320788c7abaa12b1e8e28630f0b27f
FerdRosa <fernando.a.rosa02@gmail.com>
2023-11-20 21:45:34 -0300

    Criação do grafico em Linha com multiplas seleções

--- test/src/CSV_apex.js
+++ test/src/CSV_apex.js
@@ -7,8 +7,8 @@ const CSVReader = () => {
   const [dataForApexCharts, setDataForApexCharts] = useState([]);
   const [infoData, setInfoData] = useState([]);
   const [dataColors, setDataColors] = useState({});
-  // const [selectedInfo, setSelectedInfo] = useState([]);
-  const [selectedInfo, setSelectedInfo] = useState('');
+  const [selectedInfo, setSelectedInfo] = useState([]);
+  // const [selectedInfo, setSelectedInfo] = useState('');
 
   const handleFileChosen = (file) => {
     Papa.parse(file, {
@@ -37,22 +37,17 @@ const CSVReader = () => {
         });
 
         setDataForApexCharts(formattedData);
-        // setSelectedInfo([formattedData[0].nome]); // Inicializa com o primeiro valor por padrão
-        setSelectedInfo(formattedData[0].nome); // Inicializa com o primeiro valor por padrão
+        setSelectedInfo([formattedData[0].nome]); // Inicializa com o primeiro valor por padrão
       },
       header: true,
     });
   };
 // //ESTA PARA MULTIPLAS ESCOLHAS NECESSARIO MUDAR PARA AS OUTRAS
-  // const handleInfoChange = (e) => {
-  //   const selectedValues = Array.from(e.target.selectedOptions, (option) => option.value);
-  //   setSelectedInfo(selectedValues || []); // Certifique-se de que selectedInfo seja um array
-  // };
+  const handleInfoChange = (e) => {
+    const selectedValues = Array.from(e.target.selectedOptions, (option) => option.value);
+    setSelectedInfo(selectedValues || []); // Certifique-se de que selectedInfo seja um array
+  };
 
-// //ESTA PARA MULTIPLAS ESCOLHAS NECESSARIO MUDAR PARA AS OUTRAS
-const handleInfoChange = (selectedValue) => {
-  setSelectedInfo(selectedValue);
-};
 
   useEffect(() => {
     const input = document.createElement('input');
@@ -71,46 +66,6 @@ const handleInfoChange = (selectedValue) => {
   // }
 
 // // FAZENDO O GRAFICO EM BARRA
-//   // Escolhendo apenas uma info
-      // const options = {
-      //   xaxis: {
-      //     categories: infoData.filter((header) => header !== 'nome'),
-      //   },
-      //   colors: Object.values(dataColors),
-      // };
-
-      // return (
-      //   <div>
-      //     <h2>Informação Headers:</h2>
-      //     <pre>{infoData.join(', ')}</pre>
-
-      //     {infoData.length > 0 && (
-      //       <select value={selectedInfo} onChange={(e) => handleInfoChange(e.target.value)}>
-      //         {dataForApexCharts.map((item) => (
-      //           <option key={item.nome} value={item.nome}>
-      //             {item.nome}
-      //           </option>
-      //         ))}
-      //       </select>
-      //     )}
-
-      //     <Chart
-      //       options={options}
-      //       series={[
-      //         {
-      //           name: selectedInfo,
-      //           data: infoData
-      //             .filter((header) => header !== 'nome')
-      //             .map((year) => dataForApexCharts.find((item) => item.nome === selectedInfo)[year]),
-      //         },
-      //       ]}
-      //       type="bar"
-      //       height={350}
-      //     />
-      //   </div>
-      // );
-  
-
       // const options = {
       //   xaxis: {
       //     categories: infoData.filter((header) => header !== 'nome'),
@@ -149,26 +104,86 @@ const handleInfoChange = (selectedValue) => {
 
 
 // // FAZENDO O GRAFICO EM LINHA 
+
+// const options = {
+//   chart: {
+//     height: 350,
+//     type: 'line',
+//     zoom: {
+//       enabled: false,
+//     },
+//   },
+//   dataLabels: {
+//     enabled: false,
+//   },
+//   stroke: {
+//     curve: 'straight',
+//   },
+//   title: {
+//     text: 'Grafico em Linha',
+//     align: 'left',
+//   },
+//   grid: {
+//     row: {
+//       colors: ['#f3f3f3', 'transparent'],
+//       opacity: 0.5,
+//     },
+//   },
+//   xaxis: {
+//     categories: infoData.filter((header) => header !== 'nome'),
+//   },
+// };
+
+// return (
+//   <div>
+//     <h2>Informação Headers:</h2>
+//     <pre>{infoData.join(', ')}</pre>
+
+//     {infoData.length > 0 && (
+//       <select multiple value={selectedInfo} onChange={handleInfoChange}>
+//         {dataForApexCharts.map((item) => (
+//           <option key={item.nome} value={item.nome}>
+//             {item.nome}
+//           </option>
+//         ))}
+//       </select>
+//     )}
+
+//     <Chart
+//       options={options}
+//       series={selectedInfo.map((info) => ({
+//         name: info,
+//         data: infoData.filter((header) => header !== 'nome').map((year) => dataForApexCharts.find((item) => item.nome === info)[year]),
+//       }))}
+//       type="line"
+//       height={350}
+//     />
+//   </div>
+// );
+
+  
+
+// ///GRAFICO EM PIZZA
 const options = {
   chart: {
-    id: 'line',
-    toolbar: {
-      show: false,
+    width: 380,
+    type: 'pie',
+  },
+  labels: selectedInfo,
+  responsive: [
+    {
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 200,
+        },
+        legend: {
+          position: 'bottom',
+        },
+      },
     },
-  },
-  xaxis: {
-    categories: dataForApexCharts.map((item) => item.nome),
-  },
+  ],
 };
-
-const filteredData = dataForApexCharts.filter((item) => item.nome === selectedInfo);
-
-  const series = infoData
-    .filter((header) => header !== 'nome')
-    .map((year) => ({
-      name: year,
-      data: filteredData.map((item) => item[year]),
-    }));
 
 return (
   <div>
@@ -176,52 +191,21 @@ return (
     <pre>{infoData.join(', ')}</pre>
 
     {infoData.length > 0 && (
-      <select value={selectedInfo} onChange={(e) => handleInfoChange(e.target.value)}>
-      {dataForApexCharts.map((item) => (
-        <option key={item.nome} value={item.nome}>
-          {item.nome}
-        </option>
-      ))}
-    </select>
+      <select multiple value={selectedInfo} onChange={handleInfoChange}>
+        {dataForApexCharts.map((item) => (
+          <option key={item.nome} value={item.nome}>
+            {item.nome}
+          </option>
+        ))}
+      </select>
     )}
 
-    <Chart options={options} series={series} type="line" height={600} />
+    <div id="chart">
+      <Chart options={options} series={dataForApexCharts} type="pie" width={380} />
+    </div>
+    <div id="html-dist"></div>
   </div>
 );
-
-  
-
-// ///GRAFICO EM PIZZA
-// // const options = {
-// //   labels: infoData.filter((header) => header !== 'nome'),
-// //   colors: Object.values(dataColors),
-// // };
-
-// // return (
-// //   <div>
-// //     <h2>Informação Headers:</h2>
-// //     <pre>{infoData.join(', ')}</pre>
-
-// //     {infoData.length > 0 && (
-// //       <select value={selectedInfo} onChange={(e) => handleInfoChange(e.target.value)}>
-// //         {dataForApexCharts.map((item) => (
-// //           <option key={item.nome} value={item.nome}>
-// //             {item.nome}
-// //           </option>
-// //         ))}
-// //       </select>
-// //     )}
-
-// //     <Chart
-// //       options={options}
-// //       series={dataForApexCharts.find((item) => item.nome === selectedInfo)
-// //         ? Object.values(dataForApexCharts.find((item) => item.nome === selectedInfo)).slice(1)
-// //         : []}
-// //       type="pie"
-// //       width="600"
-// //     />
-// //   </div>
-// // );
 
 };
 
