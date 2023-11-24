31fe7cdb438941657ae60e9cc7f5893081415913
carlosEduardo-hub <carloseduardo010616@gmail.com>
2023-11-24 12:21:43 -0300

    Acrescentando Logo a pagina inicial

--- src/templates/InitialPage.js
+++ src/templates/InitialPage.js
@@ -1,13 +1,19 @@
 import React from "react";
 import '../styles/InitialPage.css'
+import Logo from '../img/brasao_ufpi.png'
 
 const InitialPage = () => {
     return (
         <>
-            <div className="page">
-                <h1>Sistema PPGCC</h1>
-                <div className="button">
-                    <button>Entrar</button>
+            <div className="w-screen h-screen bg-bgcolor flex justify-center items-center flex-col gap-6">
+                <div>
+                    <img src={Logo} alt="brasao"/>
+                </div>
+                <div>
+                    <h1 className="text-5xl">Sistema PPGCC</h1>
+                </div>
+                <div className="w-screen h-7 flex justify-center">
+                    <button className="bg-secondbgcolor hover:bg-hovercolor w-20 rounded-lg font-bold">Entrar</button>
                 </div>
             </div>
         </>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bgcolor: '#404E82',
+        secondbgcolor: '#C4D2EB',
+        hovercolor: '#aab2c0'
+      }
+    },
+  },
+  plugins: [],
+}
+
