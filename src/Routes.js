06d3274d485f5869e7a78a127e45b495b7a8f066
carlosEduardo-hub <carloseduardo010616@gmail.com>
2024-01-15 12:01:12 -0300

    Iniciando Dashboard

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InitialPage from "./templates/InitialPage";
 import XLSXReader from "./XLSXTest";
+import Dashboard from "./dashboard";
 
 
 const AppRoutes = () => {
@@ -10,6 +11,7 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={<InitialPage />}></Route>
                 <Route path="/graficos" element={<XLSXReader/>}></Route>
+                <Route path="/dashboard" element={<Dashboard/>}></Route>
             </Routes>
         </Router>
     )
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { SidebarProvider } from './context/sidebarContext.jsx';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <SidebarProvider>
     <App />
-  </React.StrictMode>
+  </SidebarProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
