b5180ec659f781add2b46149ad93cdb35ef299a3
FerdRosa <fernando.a.rosa02@gmail.com>
2024-01-21 21:54:43 -0300

    Tentativa de recomeçar porem continua erro polify

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InitialPage from "./templates/InitialPage";
-import XLSXReader from "./XLSXReader";
+import XLSXReader from "./XLSXReader2";
 
 
 
